83c04b078eb8b38427eb75f0d9b3dec0622cb223
dmlim <dmlim@rankwave.net>
2017-04-28 18:29:16 +0900

    enable http multi port

--- proxy.js
+++ proxy.js
@@ -36,40 +36,49 @@ function startServer(options)
 	/*************************************************************************************************************
 	 * http server
 	 */
+	
+	function createServer(serverOptions)
+	{
+		var server = http.createServer();
 
-	var server = http.createServer();
+		server.on('checkContinue', (req, res) => {
+			console.log('server "checkContinue"');
+			res.writeContinue();
+			server.emit('request', req, res);
+		});
 
-	server.on('checkContinue', (req, res) => {
-		console.log('server "checkContinue"');
-		res.writeContinue();
-		server.emit('request', req, res);
-	});
+		server.on('connect', (req, socket, head) => {
+			if ( workerPool.getPoolSize() > 0 )
+			{
+				elasticHandler.proxyConnect(req, socket, head);
+			}
+			else
+			{
+				defaultHandler.proxyConnect(req, socket, head);
+			}
+		});
 
-	server.on('connect', (req, socket, head) => {
-		if ( workerPool.getPoolSize() > 0 )
-		{
-			elasticHandler.proxyConnect(req, socket, head);
-		}
-		else
-		{
-			defaultHandler.proxyConnect(req, socket, head);
-		}
-	});
+		server.on('request', (req, res) => {
+			if ( workerPool.getPoolSize() > 0 )
+			{
+				elasticHandler.proxyRequest(req, res);
+			}
+			else
+			{
+				defaultHandler.proxyRequest(req, res);
+			}
+		});
 
-	server.on('request', (req, res) => {
-		if ( workerPool.getPoolSize() > 0 )
-		{
-			elasticHandler.proxyRequest(req, res);
-		}
-		else
-		{
-			defaultHandler.proxyRequest(req, res);
-		}
-	});
-
-	server.listen(proxyOptions, () => {
-		console.log(`HTTP Server Started (port: ${proxyOptions.port})`);
-	});
+		server.listen(serverOptions, () => {
+			console.log(`HTTP Server Started (port: ${serverOptions.port})`);
+		});
+	}
+	
+	for ( var i = 0 ; i < proxyOptions.ports.length ; i++ )
+	{
+		var serverOptions = Object.assign({}, proxyOptions, {port: proxyOptions.ports[i]});
+		createServer(serverOptions);
+	}
 }
 
 function startWorker(options)
